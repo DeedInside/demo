02eeaf3baff0b48d4d9d4ec888e81190eb9a3f5f
PaulSA <paul_s_a@mail.ru>
2026-02-26 15:35:59 +0700

    1.0

---
+++ demo/Windows/Authorization.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace demo.Windows
+{
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_authorization(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_authorization_gouest(object sender, RoutedEventArgs e)
+        {
+            Main main = new Main();
+            main.Show();
+            this.Close();
+        }
+    }
+}
---
+++ demo/Windows/Main.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace demo.Windows
+{
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+            BoxUserName.Text = "гость";
+        }
+        public Main(int n)
+        {
+            InitializeComponent();
+        }
+
+        private void Button_exit_user(object sender, RoutedEventArgs e)
+        {
+            Authorization authorization = new Authorization();
+            authorization.Show();
+            this.Close();
+        }
+    }
+}
